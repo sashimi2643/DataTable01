d00ef1103fb0ca2cf917edcf61e0554bd8fd8b01
4D-10\koyo <a>
2026-06-02 13:42:38 +0900

    projectの作成 EnemyData.hの入力

---
+++ project1/EnemyDeta.h
@@ -0,0 +1,16 @@
+#pragma once
+//#include "Elemennt.h"
+
+
+struct EnemyData
+{
+	int ID;
+	const char Name[256];
+	int HP;
+	int ATK;
+	int DEF;
+	int SPD;
+	int Gold;
+	int EXP;
+	Element Element;
+} 
