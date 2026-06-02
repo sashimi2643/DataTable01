e5af1671d23cdf24e03b7aa08e9cc24b63210e16
4D-10\koyo <a>
2026-06-02 16:09:45 +0900

    Enemy.h の訂正

--- project1/Enemy.h
+++ project1/Enemy.h
@@ -7,28 +7,11 @@
 class Enemy
 {
 public:
-	int ID;
-	const char* Name;
-	int HP;
-	int ATK;
-	int DEF;
-	int SPD;
-	int Gold;
-	int EXP;
-	Element Element;
-	float CriticalRate;
+
+	EnemyData Data;
 
 	Enemy(const EnemyData& data)
-		: ID(data.ID),
-		Name(data.Name),
-		HP(data.HP),
-		ATK(data.ATK),
-		DEF(data.DEF),
-		SPD(data.SPD),
-		Gold(data.Gold),
-		EXP(data.EXP),
-		Element(data.Element),
-		CriticalRate(data.CriticalRate)
+		: Data(data)
 	{
 	}
 };
--- project1/Main.cpp
+++ project1/Main.cpp
@@ -7,12 +7,12 @@ int main()
 	for (int i = 1; i <= 4; i++) {
 		Enemy* enemy = EnemyFactory::CreateEnemy(i);
 
-		printf("%s EXP:%d, Gold:%d, CriticalRate:%f\n", enemy->Name, enemy->EXP, enemy->Gold, enemy->CriticalRate);
+		printf("%s EXP:%d, Gold:%d, CriticalRate:%f\n", enemy->Data.Name, enemy->Data.EXP, enemy->Data.Gold, enemy->Data.CriticalRate);
 
 		delete enemy;
 	}
 
-
+	
 
 	return 0;
 }
